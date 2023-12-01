f697775821953bbf16bc5467b7c8948c4b1c225d
Moh-Tharwat <mohamedtharwat3551@gmail.com>
2023-12-01 15:35:28 +0200

    Add existing project files to Git

---
+++ .history/Helm_charts/Postgres/init_20231128220339.sql
@@ -0,0 +1,9 @@
+CREATE TABLE auth_user (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email VARCHAR (255) NOT NULL,
+    password VARCHAR (255) NOT NULL
+);
+
+--Add Username and Password for Admin User
+-- INSERT INTO auth_user (email, password) VALUES ('anyone@gmail.com', '1');
+INSERT INTO auth_user (email, password) VALUES (mohamedtharwat3551@gmail.com, 'Mhmdt2017@');
